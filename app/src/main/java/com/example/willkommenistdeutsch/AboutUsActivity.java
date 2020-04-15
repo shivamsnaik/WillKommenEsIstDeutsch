8e82f87e646354cb63a76b0a794d59c71f69e1db
Shivam Naik <shivamsnaik@gmail.com>
2020-04-16 00:25:47 +0530

    Added code for custom font style in About Us activity and changed git link

--- app/src/main/java/com/example/willkommenistdeutsch/AboutUsActivity.java
+++ app/src/main/java/com/example/willkommenistdeutsch/AboutUsActivity.java
@@ -2,14 +2,28 @@ package com.example.willkommenistdeutsch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
+
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class AboutUsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ViewPump.init(ViewPump.builder()
+                .addInterceptor(new CalligraphyInterceptor(
+                        new CalligraphyConfig.Builder()
+                                .setDefaultFontPath("fonts/Gidole-Regular.otf")
+                                .setFontAttrId(R.attr.fontPath)
+                                .build()))
+                .build());
 
         if(MainActivity.themeToggle)
         {
@@ -18,5 +32,11 @@ public class AboutUsActivity extends AppCompatActivity {
             this.setTheme(R.style.LightTheme);
 
         setContentView(R.layout.activity_about_us);
+    }
+
+    //ATTACHES NEW CONTEXT TO REFLECT CUSTOM FONT STYLE
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
 }
